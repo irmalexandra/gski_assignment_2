5b9dde51df931f3bf7066ad193521ca840b9fa32
lokialex <lokih@ru.is>
2020-02-07 14:25:29 +0000

    Finished deque

---
+++ my_array_deque.py
@@ -0,0 +1,101 @@
+
+class ArrayDeque():
+    def __init__(self):
+        self.capacity = 4
+        self.arr = [0]*self.capacity
+        self.front = 0
+        self.size = 0
+
+    def __str__(self):
+        pass
+
+    def pushFront(self, data):
+        if self.size == self.capacity:
+            self.__resize()
+
+        front = (self.front -1) % self.capacity
+        self.arr[front] = data
+        self.front -= 1
+        self.size += 1
+    
+    def pushBack(self, data):
+        if self.size == self.capacity:
+            self.__resize()
+
+        back = (self.front + self.size) % self.capacity
+
+        self.arr[back] = data
+        self.size +=1
+
+    def popFront(self):
+        returnVal = self.arr[self.front]
+        self.arr[self.front] = None
+
+        self.front = (self.front + 1) % self.capacity
+        
+        self.size -= 1
+
+        return returnVal
+
+    def popBack(self):
+        returnVal = self.arr[self.size]
+        back = (self.front + self.size -1) % self.capacity
+        self.arr[back] = None
+        
+        self.size -= 1
+
+        return returnVal
+
+    def getSize(self):
+        return self.size
+    
+    def __resize(self):
+
+        self.capacity *= 2
+        new_arr = [0]*self.capacity
+
+        index = self.front
+        for i in range(self.size):
+
+            if index == self.size:
+                
+                new_arr[i] = self.arr[i - (index-self.front)]
+            
+            else:
+                new_arr[i] = self.arr[index]
+                index += 1                 
+        self.front = 0
+        self.arr = new_arr
+
+    
+new_arr = ArrayDeque()
+
+for i in range(8):
+    new_arr.pushBack("string " + str(i+1))
+
+for i in range(4):
+    new_arr.popFront()
+
+new_arr.pushBack("tester 1")
+new_arr.pushBack("tester 2")
+new_arr.pushBack("tester 3")
+new_arr.pushBack("tester 4")
+
+new_arr.pushBack("tester resize?")
+print(new_arr.popFront())
+new_arr.pushBack("tester")
+new_arr.pushBack("tester")
+
+new_arr.popBack()
+new_arr.popBack()
+new_arr.popFront()
+
+new_arr.pushFront("frontTest1")
+
+for i in range(8):
+    new_arr.pushBack("pushbackstring{}".format(i))
+
+new_arr.pushBack("resize test 2")
+
+print()
+
